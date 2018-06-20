22d549102f35722627a8ae503abc284760fc804b
Miguel Torres <migueltg93@gmail.com>
2018-06-20 08:40:14 -0500

    added styles

--- app/javascript/components/app.vue
+++ app/javascript/components/app.vue
@@ -15,8 +15,64 @@ export default {
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
+body {
+  background-color: #fff;
+  color: #fff;
   text-align: center;
+  margin: 0px;
+}
+
+#app {
+  margin: 0 auto;
+  max-width: 270px;
+  color: #34495e;
+}
+
+h1 {
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 3em;
+}
+
+.restart {
+  background-color: #e74c3c;
+  color: #fff;
+  border: 0px;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  font-size: 1.4em;
+  font-weight: bold;
+  margin: 0px;
+  padding: 15px;
+  width: 100%;
+}
+
+.restart:hover {
+  background-color: #c0392b;
+  cursor: pointer;
+}
+
+.scoreBoard {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 15px;
+  background-color: #16a085;
+  box-shadow: 10px solid #fff;
+  padding: 20px;
+  overflow: none;
+}
+
+.scoreBoard h2 {
+  margin: 0px;
+}
+
+.scoreBoard span {
+  float: right;
+  font-size: 1.5em;
+  font-weight: bold;
+  margin-left: 20px;
 }
 </style>
---
+++ app/javascript/components/cell.vue
@@ -0,0 +1,43 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+.cell {
+  width: 33.333%;
+  height: 90px;
+  border: 6px solid #2c3e50;
+  font-size: 3.5em;
+}
+
+.cell:hover {
+  background-color: #7f8c8d;
+}
+
+.cell::after {
+  content: '';
+  display: block;
+}
+
+.cell:first-of-type {
+  border-left-color: transparent;
+  border-top-color: transparent;
+}
+
+.cell:nth-of-type(2) {
+  border-left-color: transparent;
+}
+
+.cell:nth-of-type(3) {
+  border-left-color: transparent;
+}
+
+tr:nth-of-type(3) .cell {
+  border-bottom-color: transparent;
+}
+</style>
